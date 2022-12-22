53a658965ee6b2409b6fc8c1de317f8ee01a104e
shutnik42 <118187272+shutnik42@users.noreply.github.com>
2022-12-22 15:23:51 +0300

    task003 solution

---
+++ TASK003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача №3
+//Найти максимальное из трех чисел
+
+Console.Write("Введите первое число ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите третье число ");
+int c = int.Parse(Console.ReadLine()!);
+
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine("Максимальное число: " + max);
