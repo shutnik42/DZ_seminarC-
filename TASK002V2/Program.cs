bf487a8c741d95e1e1b21ea2e16dd7478ed78ee0
shutnik42 <118187272+shutnik42@users.noreply.github.com>
2022-12-22 15:14:31 +0300

    task002 var2 solution

---
+++ TASK002V2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача № 2
+// По заданному номеру дня недели вывести его название
+
+string[] Days = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+Console.Write("Задайте день недели -> ");
+int numberDays = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(Days[numberDays - 1]);
