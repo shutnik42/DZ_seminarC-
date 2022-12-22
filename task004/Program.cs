0f15c8d640e24bea28ccbc3a2c0d2aef6ddc1274
shutnik42 <118187272+shutnik42@users.noreply.github.com>
2022-12-22 15:29:37 +0300

    task004 solution

---
+++ task004/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача №4
+// Выяснить является ли число чётным
+
+Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Это число четное!");
+}
+else
+{
+    Console.WriteLine("Это НЕ четное число!");
+}
